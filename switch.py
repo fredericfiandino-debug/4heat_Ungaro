d384ed8e0fd6f65667feb0ad0c03c1b4c6d8bd5e
fredericfiandino-debug <frederic.fiandino@gmail.com>
2026-07-11 19:36:41 +0200

    Refactor 4Heat integration to add 4heatlite switch
    Updated the 4Heat integration to include a new 4heatlite switch implementation, replacing the previous 4Heat switch functionality.

--- switch.py
+++ switch.py
@@ -1,86 +1,75 @@
-"""The 4Heat integration."""
+"""The 4heatlite integration - switch."""
 
 import logging
+
 from homeassistant.components.switch import SwitchEntity
 from homeassistant.helpers.update_coordinator import CoordinatorEntity
 
-from .const import (
-    MODE_NAMES, SENSOR_TYPES, DOMAIN, DATA_COORDINATOR, MODE_TYPE, ERROR_TYPE
-)
+from .const import DOMAIN, DATA_COORDINATOR
 from .coordinator import FourHeatDataUpdateCoordinator
 
 _LOGGER = logging.getLogger(__name__)
 
+# Codes d'etat (table lingue_stati / MODE_NAMES) consideres comme "poele en marche".
+# Testes en conditions reelles : 0 (Off) et 7 (Extinguishing) -> off ; 1 (Check Up) -> on.
+# Les autres (2 Ignition, 3 Stabilization, 4 Retry Ignition, 5 Run, 6 Modulation,
+# 10 Recover Ignition, 11 Standby) sont deduits de la table du FileMap mais pas
+# encore observes chez vous -> a ajuster si besoin une fois testes.
+STATES_ON = {1, 2, 3, 4, 5, 6, 10, 11}
+
 
 async def async_setup_entry(hass, entry, async_add_entities):
-    """Add an FourHeat entry."""
+    """Add the 4heatlite on/off switch."""
     coordinator: FourHeatDataUpdateCoordinator = hass.data[DOMAIN][entry.entry_id][
         DATA_COORDINATOR
     ]
-    entities = []
-
-    for sensorId in coordinator.swiches:
-        try:
-            entities.append(FourHeatSwitch(coordinator, sensorId, entry.title))
-        except:
-            _LOGGER.debug(f"Error adding {sensorId}")
-
-    async_add_entities(entities)
+    async_add_entities([FourHeatLiteSwitch(coordinator, entry.title)])
 
 
-class FourHeatSwitch(CoordinatorEntity, SwitchEntity):
-    """Representation of a 4Heat device."""
+class FourHeatLiteSwitch(CoordinatorEntity, SwitchEntity):
+    """Representation of the 4heatlite on/off switch."""
 
-    def __init__(self, coordinator, sensor_type, name):
-        """Initialize the sensor."""
+    def __init__(self, coordinator, name):
+        """Initialize the switch."""
         super().__init__(coordinator)
-        self._sensor = SENSOR_TYPES[sensor_type][0]
-        self._name = name
-        self.type = sensor_type
         self.coordinator = coordinator
-        self._last_value = None
+        self._name = name
         self.serial_number = coordinator.serial_number
         self.model = coordinator.model
-        _LOGGER.debug(self.coordinator)
 
     @property
     def name(self):
-        """Return the name of the sensor."""
-        return f"{self._name} {self._sensor}"
+        """Return the name of the switch."""
+        return f"{self._name} Marche/Arrêt"
+
+    @property
+    def icon(self):
+        """Return icon."""
+        return "mdi:radiator"
 
     @property
     def is_on(self):
-        """Return true if switch is on."""
-        if self.type not in self.coordinator.data: 
+        """Return true if the stove is running (etat dans STATES_ON)."""
+        if not self.coordinator.data or "mode_code" not in self.coordinator.data:
             return False
-        if self.type == MODE_TYPE:
-            return self.coordinator.data[self.type][0] not in [0,256,7,8,9]
-        elif self.type == ERROR_TYPE:
-            return self.coordinator.data[self.type][0] != 0
+        return self.coordinator.data["mode_code"] in STATES_ON
 
     async def async_turn_on(self, **kwargs):
-        """Turn the switch on."""
-        if self.type == MODE_TYPE:
-            await self.coordinator.async_turn_on()
-        elif self.type == ERROR_TYPE:
-            None
+        """Turn the stove on."""
+        await self.coordinator.async_turn_on()
         await self.coordinator.async_request_refresh()
         self.async_write_ha_state()
 
     async def async_turn_off(self, **kwargs):
-        """Turn the switch off."""
-        if self.type == MODE_TYPE:
-            await self.coordinator.async_turn_off()
-        elif self.type == ERROR_TYPE:
-            await self.coordinator.async_unblock()
+        """Turn the stove off."""
+        await self.coordinator.async_turn_off()
         await self.coordinator.async_request_refresh()
         self.async_write_ha_state()
 
-
     @property
     def unique_id(self):
-        """Return unique id based on device serial and variable."""
-        return f"{self._name}_{self.type}"
+        """Return unique id based on device name."""
+        return f"{self._name}_switch"
 
     @property
     def device_info(self):
@@ -88,25 +77,16 @@ class FourHeatSwitch(CoordinatorEntity, SwitchEntity):
         return {
             "identifiers": {(DOMAIN, self.serial_number)},
             "name": self._name,
-            "manufacturer": "4Heat",
+            "manufacturer": "Tiemme",
             "model": self.model,
         }
 
     @property
-    def state_attributes(self):
-        try:
-            if self.type == MODE_TYPE:
-                val = self.coordinator.data[self.type][0]
-                return {
-                    "Num Val": val,
-                    "Val text": MODE_NAMES.get(val, "unknown-value")
-                }
-            elif self.type == ERROR_TYPE:
-                return {"Num Val": self.coordinator.data[self.type][0]}
-            else:
-                return None
-
-        except Exception as ex:
-            _LOGGER.debug("exception in state attributes")
-            _LOGGER.error(ex)
+    def extra_state_attributes(self):
+        """Return the current mode (texte + code) as attributes."""
+        if not self.coordinator.data:
             return None
+        return {
+            "mode": self.coordinator.data.get("mode"),
+            "mode_code": self.coordinator.data.get("mode_code"),
+        }
